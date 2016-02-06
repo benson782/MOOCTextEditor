1eab05e20d7b4518c80fe8ced48d0e96e82d3817
Benson Malonzo <benson782@hotmail.com>
2016-02-05 18:17:17 -0800

    Week 3 Mod 1
    Implement MyLinkedListTester junit tests

--- src/textgen/MyLinkedListTester.java
+++ src/textgen/MyLinkedListTester.java
@@ -109,12 +109,51 @@ public class MyLinkedListTester {
 	@Test
 	public void testRemove()
 	{
+		
+		// test out of bounds
+		try {
+			shortList.remove(-1);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		try {
+			shortList.remove(2);
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+		
+		// remove beginning
 		int a = list1.remove(0);
 		assertEquals("Remove: check a is correct ", 65, a);
 		assertEquals("Remove: check element 0 is correct ", (Integer)21, list1.get(0));
 		assertEquals("Remove: check size is correct ", 2, list1.size());
 		
-		// TODO: Add more tests here
+		// remove at end
+		int newEndData = list1.get(list1.size() - 2);
+		int endData = list1.get(list1.size() - 1);
+		int endRemoved = list1.remove(list1.size() - 1);
+		assertEquals("Remove: end removed is correct ", endData, endRemoved);
+		assertEquals("Remove: check new end data ", (Integer)newEndData, list1.get(list1.size() - 1));
+		assertEquals("Remove: check size is correct ", 1, list1.size());
+		
+		// add some more to the list
+		list1.add(90);
+		list1.add(92);
+		list1.add(93);
+		list1.add(99);
+		list1.add(98);
+		
+		// remove from middle
+		int currentSize = list1.size();
+		int middleIndex = list1.size() / 2;
+		int newMiddleData = list1.get(middleIndex + 1);
+		int middeData = list1.get(middleIndex);
+		int middleRemoved = list1.remove(middleIndex);
+		assertEquals("Remove: end middle is correct ", middeData, middleRemoved);
+		assertEquals("Remove: check new middle data ", (Integer)newMiddleData, list1.get(middleIndex));
+		assertEquals("Remove: check size is correct ", currentSize - 1, list1.size());
+		
 	}
 	
 	/** Test adding an element into the end of the list, specifically
@@ -123,8 +162,16 @@ public class MyLinkedListTester {
 	@Test
 	public void testAddEnd()
 	{
-        // TODO: implement this test
+		// adding null
+		try {
+			shortList.add(null);
+			fail("Check adding null");
+		}
+		catch (NullPointerException e) {}
 		
+		// add to end
+		list1.add(100);
+		assertEquals("Add End: 100 is correct ", (Integer)100, list1.get(list1.size() - 1));
 	}
 
 	
@@ -133,6 +180,20 @@ public class MyLinkedListTester {
 	public void testSize()
 	{
 		// TODO: implement this test
+		assertEquals("Size: empty list is 0 ", 0, emptyList.size());
+		
+		// test on list1 size
+		int currentSize = list1.size();
+		
+		// add to list1
+		list1.add(1000);
+		currentSize++;
+		assertEquals("Size: size increment ", currentSize, list1.size());
+		
+		// remove from list1
+		list1.remove(list1.size() - 1);
+		currentSize--;
+		assertEquals("Size: size decrement ", currentSize, list1.size());
 	}
 
 	
@@ -144,7 +205,47 @@ public class MyLinkedListTester {
 	@Test
 	public void testAddAtIndex()
 	{
-        // TODO: implement this test
+		// test out of bounds
+		try {
+			shortList.add(-1, "Negative");
+			fail("Check out of bounds under");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		try {
+			shortList.add(shortList.size()+1, "Over");
+			fail("Check out of bounds over");
+		}
+		catch (IndexOutOfBoundsException e) {}
+		
+		// test adding null
+		try {
+			shortList.add(0, null);
+			fail("Check adding null");
+		}
+		catch (NullPointerException e) {}
+		
+		// add to empty at index 0
+		emptyList.add(0, 69);
+		assertEquals("Add At Index 0: 69 is correct ", (Integer)69, emptyList.get(0));
+		assertEquals("Remove At Index 0: 69 is correct ", (Integer)69, emptyList.remove(0));
+		
+		// add to beginning
+		list1.add(0, 69);
+		assertEquals("Add At Index 0: 69 is correct ", (Integer)69, list1.get(0));
+		
+		// add to end
+		list1.add(list1.size() - 1, 169);
+		Integer valAtEnd = list1.get(list1.size() - 2); 
+		assertEquals("Add At Index end: 169 is correct ", (Integer)169, valAtEnd);
+		
+		// add to middle
+		int middleIndex = list1.size() / 2;
+		int middleIndexOldVal = list1.get(middleIndex);
+		list1.add(middleIndex, 269);
+		assertEquals("Add At Index " + middleIndex + ": 269 is correct ", (Integer)269, list1.get(middleIndex));
+		assertEquals("Add At Index " + (middleIndex+1) + ": "+ middleIndexOldVal + " is correct ", 
+				(Integer)middleIndexOldVal, list1.get(middleIndex+1));
 		
 	}
 	
@@ -152,8 +253,31 @@ public class MyLinkedListTester {
 	@Test
 	public void testSet()
 	{
-	    // TODO: implement this test
-	    
+		// test out of bounds
+		try {
+			shortList.set(-1, "Negative");
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+
+		try {
+			shortList.set(2, "Over");
+			fail("Check out of bounds");
+		}
+		catch (IndexOutOfBoundsException e) {}
+		
+		// test setting null
+		try {
+			shortList.set(0, null);
+			fail("Check setting null");
+		}
+		catch (NullPointerException e) {}
+		
+	    // set middle element
+		int middleIndex = list1.size() / 2;
+		int oldVal = list1.get(middleIndex);
+		int oldVal2 = list1.set(middleIndex, 20000);
+		assertEquals("Set: old data and data returned", oldVal, oldVal2);
 	}
 	
 	
