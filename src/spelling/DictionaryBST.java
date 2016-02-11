a75fe3527d128e4f1917d4c62015c1190bd3306d
Benson Malonzo <benson782@hotmail.com>
2016-02-11 14:35:43 -0800

    Week 4
    Implement DictionaryBST

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -10,8 +10,12 @@ public class DictionaryBST implements Dictionary
 {
    private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
+   /** 
+    * Constructor
+    */
+   public DictionaryBST() {
+	   dict = new TreeSet<>();
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,7 +24,11 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
+    	String wordLowerCase = word.toLowerCase();
+    	if (!dict.contains(wordLowerCase)) {
+    		dict.add(wordLowerCase);
+    		return true;
+    	}
         return false;
     }
 
@@ -28,14 +36,12 @@ public class DictionaryBST implements Dictionary
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+        return dict.contains(s.toLowerCase());
     }
 
 }
